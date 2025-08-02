3563c30688c9a4f613470d31fda0c710120d8e40
Dhruv Rekhawat <hellodhruvr@gmail.com>
2025-08-02 20:51:14 +0530

    set up monorepo

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,11 +2,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
-import tailwindcss from "@tailwindcss/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   base: '/chataiagent/', // Ensures assets are pathed correctly for the subfolder
   server:{
     allowedHosts: ["bc7d-2401-4900-1c3d-4d6a-bd5b-de75-4037-927a.ngrok-free.app"] // Remove or comment out for production builds
